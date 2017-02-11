8396a5cdcf0194086924700c7d09dd4834a8b465
Jerry Yu <jerryyu8@gmail.com>
2017-02-10 20:43:24 -0500

    add rama's good functions

--- src/neuralNet2Layer.js
+++ src/neuralNet2Layer.js
@@ -9,6 +9,14 @@ function deriv(y) {
     return y * (1-y)
 }
 
+// both arrays must be of the same length
+function multiplyElements(arr1, arr2) {
+    arr1.forEach(function(index) {
+        arr1[index] = arr1[index] * arr2[index];
+    })
+    return arr1
+}
+
 // Input Data
 const x = math.matrix([[0, 0, 1],
                      [0, 1, 1],
@@ -32,8 +40,8 @@ let l2_delta
 
 for (let i = 0; i < 60000; i++) {
     // forward propagation
-    l1 = sigmoid(math.multiply(l0, syn0))
-    l2 = sigmoid(math.multiply(l0, syn0))
+    l1 = math.multiply(l0, syn0).map(sigmoid)
+    l2 = math.multiply(l1, syn1).map(sigmoid)
 
     // how much did we miss?
     l2_error = math.subtract(y, l2)
@@ -43,11 +51,15 @@ for (let i = 0; i < 60000; i++) {
 
     l1_error = math.multiply(l2_delta, math.transpose(syn1))
 
-    l1_delta = math.multiply(l1_error, deriv(l1)) // multiply every element
+    l1_delta = math.multiply(l1_error, l1.map(deriv)) // multiply every element
     // update weights
-    syn1 = math.add(math.multiply(math.transpose(l0),l2_delta), syn1)
-    syn0 = math.add(math.multiply(math.transpose(l0),l1_delta), syn0)
+    let t0 = math.transpose(l0)
+    let m0 = math.multiply(t0, l1_delta)
+    syn0 = math.add(syn0, m0)
 
+    let t1 = math.transpose(l1)
+    let m1 = math.multiply(t1, l2_delta)
+    syn1 = math.add(syn1, m)
 }
 
-console.log(l1)
+console.log(l2)
