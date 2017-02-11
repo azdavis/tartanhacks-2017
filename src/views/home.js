2a517e2f8f6d564c8cdeff95fee7295218f58cc2
azdavis <ariel.z.davis@icloud.com>
2017-02-10 21:01:30 -0500

    change the style a bit

--- src/views/home.js
+++ src/views/home.js
@@ -4,36 +4,26 @@ const msg2 = document.getElementById("msg2")
 const form = document.getElementById("form")
 const input = document.getElementById("input")
 
-function one() {
-    form.onsubmit = e => {
-        e.preventDefault()
-        input.value = ""
-        two()
-    }
-}
-
-function two() {
+const one = e => {
+    e.preventDefault()
     msg.innerHTML = "what are some factors that affect that question?"
     msg2.innerHTML = "separate them with commas (',')."
-    form.onsubmit = e => {
-        e.preventDefault()
-        const factors = input.value.split(",").map(x => x.trim())
-        if (factors.length > 10) {
-            msg2.innerHTML = "that's too many factors."
-            return
-        }
-        if (factors.find(x => x === "") !== undefined) {
-            msg2.innerHTML = "you can't have an empty factor."
-            return
-        }
-        form.remove()
-        three(factors)
-    }
+    input.value = ""
+    form.onsubmit = two
 }
 
-function three(factors) {
-    msg.innerHTML = "what the heck"
-    console.log(factors)
+const two = e => {
+    e.preventDefault()
+    const factors = input.value.split(",").map(x => x.trim())
+    if (factors.length > 10) {
+        msg2.innerHTML = "that's too many factors."
+        return
+    }
+    if (factors.find(x => x === "") !== undefined) {
+        msg2.innerHTML = "you can't have an empty factor."
+        return
+    }
+    form.remove()
 }
 
 title.innerHTML = "machine learning™"
@@ -41,4 +31,4 @@ msg.innerHTML = "what is your question for today?"
 msg2.innerHTML = "it should be a yes/no question."
 input.focus()
 form.style.display = "block"
-one()
+form.onsubmit = one
