b355b40f8b4b680e3d5d8cd68d1a8c13d9319ecd
Jerry Yu <jerryyu8@gmail.com>
2017-02-11 11:32:22 -0500

    revert neuralnet2layer
    to b0584bbbcba921edc969173f2316eaad38b66d6a

--- src/neuralNet2Layer.js
+++ src/neuralNet2Layer.js
@@ -61,19 +61,13 @@ let layer1_delta
 let layer2
 let layer2_error
 let layer2_delta
-let layer3
-let layer3_error
-let layer3_delta
-
 let hiddenLayerSize
 let error
 let minError = 100
 let synapse0
 let synapse1
-let synapse2
 let minSynapse0
 let minSynapse1
-let minSynapse2
 
 const hlSizes = [(x.size())[1], (x.size())[1] - 1, (x.size())[1] + 1]
 const alphas = [0.1, 1, 10]
@@ -89,8 +83,7 @@ for (let alphaIndex = 0; alphaIndex < 3; alphaIndex++) {
         // Weights
         hiddenLayerSize = hlSizes[hlSizeIndex]
         synapse0 = math.random([x.size()[1], hiddenLayerSize], -1, 1)
-        synapse1 = math.random([hiddenLayerSize, hiddenLayerSize], -1, 1)
-        synapse2 = math.random([hiddenLayerSize, 1], -1, 1)
+        synapse1 = math.random([hiddenLayerSize, 1], -1, 1)
 
         for (let i = 0; i < 40000; i++) {
             // forward propagation
@@ -103,13 +96,8 @@ for (let alphaIndex = 0; alphaIndex < 3; alphaIndex++) {
                 layer1[index] *= 1.0/(1-dropout_percent)
             })
             layer2 = math.multiply(layer1, synapse1).map(sigmoid)
-            layer3 = math.multiply(layer2, synapse2).map(sigmoid)
             // compare estimate with actual output
-            layer3_error = math.subtract(y, layer3)
-            // use slope of sigmoid to update values
-            layer3_delta = multiplyElements(layer3_error, layer3.map(deriv))
-            // distribute error over synapse 2
-            layer2_error = math.multiply(layer3_delta, math.transpose(synapse2))
+            layer2_error = math.subtract(y, layer2)
             // use slope of sigmoid to update values
             layer2_delta = multiplyElements(layer2_error, layer2.map(deriv))
             // distribute error over synapse 1
@@ -120,8 +108,6 @@ for (let alphaIndex = 0; alphaIndex < 3; alphaIndex++) {
             // update weights
             synapse0 = math.add(synapse0, math.multiply(math.transpose(layer0), layer1_delta))
             synapse1 = math.add(synapse1, math.multiply(math.transpose(layer1), layer2_delta))
-            synapse2 = math.add(synapse2, math.multiply(math.transpose(layer2), layer3_delta))
-
             if(i % 10000 == 0) {
                 if (i == 0) {
                     console.log(count)
@@ -131,14 +117,11 @@ for (let alphaIndex = 0; alphaIndex < 3; alphaIndex++) {
                     console.log(i)
             }
         }
-        let currentError = getAveError(layer3_error)
+        let currentError = getAveError(layer2_error)
         if (currentError < minError) {
             minError = currentError
-            minSynapse2 = synapse2
             minSynapse1 = synapse1
             minSynapse0 = synapse0
-            console.log("New min error")
-            console.log(minError)
         }
     }
 }
@@ -147,38 +130,31 @@ console.log("Min Error")
 console.log(minError)
 
 console.log("Output of training data after training")
-let test1 = math.multiply(x, minSynapse0).map(sigmoid)
-let test2 = math.multiply(test1, minSynapse1).map(sigmoid)
-console.log(math.multiply(test2, minSynapse2).map(sigmoid))
+let test = math.multiply(x, minSynapse0).map(sigmoid)
+console.log(math.multiply(test, minSynapse1).map(sigmoid))
 
 // Test Cases for function
 let testX = math.matrix([[0, 1, 1, 1]])
 console.log("Output of [0, 1, 1, 1]")
 let testl1 = math.multiply(testX, minSynapse0).map(sigmoid)
-let testl2 = math.multiply(testl1, minSynapse1).map(sigmoid)
-console.log(math.multiply(testl2, minSynapse2).map(sigmoid))
+console.log(math.multiply(testl1, minSynapse1).map(sigmoid))
 
 testX = math.matrix([[0, 0, 1, 1]])
 console.log("Output of [0, 0, 1, 1]")
 testl1 = math.multiply(testX, minSynapse0).map(sigmoid)
-testl2 = math.multiply(testl1, minSynapse1).map(sigmoid)
-console.log(math.multiply(testl2, minSynapse2).map(sigmoid))
+console.log(math.multiply(testl1, minSynapse1).map(sigmoid))
 
 testX = math.matrix([[1, 0, 1, 1]])
 console.log("Output of [1, 0, 1, 1]")
 testl1 = math.multiply(testX, minSynapse0).map(sigmoid)
-testl2 = math.multiply(testl1, minSynapse1).map(sigmoid)
-console.log(math.multiply(testl2, minSynapse2).map(sigmoid))
+console.log(math.multiply(testl1, minSynapse1).map(sigmoid))
 
 testX = math.matrix([[1, 1, 1, 1]])
 console.log("Output of [1, 1, 1, 1]")
 testl1 = math.multiply(testX, minSynapse0).map(sigmoid)
-testl2 = math.multiply(testl1, minSynapse1).map(sigmoid)
-console.log(math.multiply(testl2, minSynapse2).map(sigmoid))
+console.log(math.multiply(testl1, minSynapse1).map(sigmoid))
 
 console.log("Final Synapse0")
 console.log(minSynapse0)
 console.log("Final Synapse1")
 console.log(minSynapse1)
-console.log("Final Synapse2")
-console.log(minSynapse2)
