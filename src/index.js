a48f3a2e599159a79233b6d7c84c5f35da54d9b6
azdavis <ariel.z.davis@icloud.com>
2017-02-10 19:37:02 -0500

    add some basic pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello tartanhacks-2017")
+const express = require("express")
+
+const app = express()
+app.set("view engine", "pug")
+app.set("views", "./src/views")
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.use((req, res) => {
+    res.status(404).render("error", {message: "HTTP 404 Not Found"})
+})
+
+app.listen(3000)
