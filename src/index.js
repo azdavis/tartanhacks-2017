8b657b8126569bb53a046ee3118a47141e64c5a4
azdavis <ariel.z.davis@icloud.com>
2017-02-11 11:06:59 -0500

    send the right things

--- src/index.js
+++ src/index.js
@@ -15,14 +15,13 @@ app.get("/", (req, res) => {
 
 app.post("/train", (req, res) => {
     const {d, r} = req.body
-    // const {synapse0: s0, synapse1: s1} = neuralNet.train(d, r)
-    res.send({s0: 1, s1: 2})
+    const {synapse0, synapse1} = neuralNet.train(d, r)
+    res.send({s0: synapse0._data, s1: synapse1._data})
 })
 
 app.post("/get", (req, res) => {
     const {d, s0, s1} = req.body
-    // res.send(neuralNet.get(d, s0, s1))
-    res.send({r: 0.9})
+    res.send({r: neuralNet.get(d, s0, s1)._data[0]})
 })
 
 app.use((req, res) => {
