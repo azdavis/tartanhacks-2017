233123db9b1d3dcc03ab4a2b8cf502838c01da52
azdavis <ariel.z.davis@icloud.com>
2017-02-10 20:15:05 -0500

    use a form and inputs

--- src/views/home.js
+++ src/views/home.js
@@ -1,2 +1,24 @@
 const title = document.getElementById("title")
 const msg = document.getElementById("msg")
+const form = document.getElementById("form")
+
+function one() {
+    title.innerHTML = "machine learning™"
+    msg.innerHTML = "what is your question for today?"
+
+    const input = document.createElement("input")
+    form.appendChild(input)
+    form.onsubmit = e => {
+        e.preventDefault()
+        input.remove()
+        two()
+    }
+    input.focus()
+}
+
+function two() {
+    title.innerHTML = "what up"
+    msg.innerHTML = "hey"
+}
+
+one()
