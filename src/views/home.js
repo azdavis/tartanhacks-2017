0780d57b5bcffd82335bfd06f9d2354d21f98739
azdavis <ariel.z.davis@icloud.com>
2017-02-10 20:32:23 -0500

    use a msg2

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,12 @@
 const title = document.getElementById("title")
 const msg = document.getElementById("msg")
+const msg2 = document.getElementById("msg2")
 const form = document.getElementById("form")
 const input = document.getElementById("input")
 
 function one() {
     msg.innerHTML = "what is your question for today?"
+    msg2.innerHTML = "it should be a yes/no question."
     form.onsubmit = e => {
         e.preventDefault()
         input.value = ""
