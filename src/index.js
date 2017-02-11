b342f68018954ec19c71770303302e6adc2fa0cc
azdavis <ariel.z.davis@icloud.com>
2017-02-11 10:45:27 -0500

    use test data

--- src/index.js
+++ src/index.js
@@ -15,8 +15,14 @@ app.get("/", (req, res) => {
 
 app.post("/train", (req, res) => {
     const {d, r} = req.body
-    const {synapse0, synapse1} = neuralNet.train(d, r)
-    res.send({s0: synapse0._data, s1: synapse1._data})
+    // const {synapse0: s0, synapse1: s1} = neuralNet.train(d, r)
+    res.send({s0: 1, s1: 2})
+})
+
+app.post("/get", (req, res) => {
+    const {d, s0, s1} = req.body
+    // res.send(neuralNet.get(d, s0, s1))
+    res.send({r: 0.9})
 })
 
 app.use((req, res) => {
