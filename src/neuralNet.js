2d05e21635914fbf34753d15177055612ab326c7
Jerry Yu <jerryyu8@gmail.com>
2017-02-10 22:07:13 -0500

    Add error calculation

---
+++ src/neuralNet.js
@@ -0,0 +1,52 @@
+const math = require('mathjs')
+
+// Math functions for nonlinearity
+function sigmoid(val) {
+    return 1/(1+math.pow(math.e, -val))
+}
+
+// derivative of sigmoid
+function deriv(val) {
+    return val * (1-val)
+}
+
+// both arrays must be of the same length
+function multiplyElements(arr1, arr2) {
+    arr1.forEach(function(index) {
+        arr1[index] = arr1[index] * arr2[index];
+    })
+    return arr1
+}
+
+// TODO: make a function that sets x and y
+
+// Input Data
+const x = math.matrix([[0, 0, 1],
+                     [0, 1, 1],
+                     [1, 0, 1],
+                     [1, 1, 1]])
+
+// Output Data
+const y = math.transpose(math.matrix([[0, 0, 1, 1]]))
+
+// Weights
+let synapse0 = math.random([3,1], -1, 1)
+
+const layer0 = x
+let layer1
+let layer1_error
+let layer1_delta
+
+for (let i = 0; i < 60000; i++) {
+    // forward propagation
+    layer1 = math.multiply(layer0, synapse0).map(sigmoid)
+    // compare estimate with actual output
+    layer1_error = math.subtract(y, layer1)
+    // use slope of sigmoid to update values
+    layer1_delta = multiplyElements(layer1_error, layer1.map(deriv))
+    // update weights
+    synapse0 = math.add(synapse0, math.multiply(math.transpose(layer0), layer1_delta))
+}
+
+// final estimates
+console.log(layer1)
--- src/neuralNet2Layer.js
+++ src/neuralNet2Layer.js
@@ -18,6 +18,16 @@ function multiplyElements(arr1, arr2) {
     return arr1
 }
 
+// must be vertical matrix
+function getAveError(m1) {
+    let sum = 0
+    m1 = math.transpose(m1)
+    let len = (m1.size())[1]
+    for (let i = 0; i < len; i++) {
+        sum += m1[0][i]
+    }
+    return sum / len
+}
 // TODO: make a function that sets x and y
 
 // Input Data
@@ -29,10 +39,6 @@ const x = math.matrix([[0, 0, 1],
 // Output Data
 const y = math.transpose(math.matrix([[0, 0, 1, 1]]))
 
-// Weights
-const hiddenLayerSize = 4
-let synapse0 = math.random([3, hiddenLayerSize], -1, 1)
-let synapse1 = math.random([hiddenLayerSize, 1], -1, 1)
 
 const layer0 = x
 let layer1
@@ -41,23 +47,39 @@ let layer1_delta
 let layer2
 let layer2_error
 let layer2_delta
+let synapse0
+let syanpse1
+let hiddenLayerSize
+let error
 
-for (let i = 0; i < 60000; i++) {
-    // forward propagation
-    layer1 = math.multiply(layer0, synapse0).map(sigmoid)
-    layer2 = math.multiply(layer1, synapse1).map(sigmoid)
-    // compare estimate with actual output
-    layer2_error = math.subtract(y, layer2)
-    // use slope of sigmoid to update values
-    layer2_delta = multiplyElements(layer2_error, layer2.map(deriv))
-    // distribute error over synapse 1
-    layer1_error = math.multiply(layer2_delta, math.transpose(synapse1))
-    // use slope of sigmoid to update values
-    layer1_delta = multiplyElements(layer1_error, layer1.map(deriv))
-    // compare estimate with actual output
-    // update weights
-    synapse0 = math.add(synapse0, math.multiply(math.transpose(layer0), layer1_delta))
-    synapse1 = math.add(synapse1, math.multiply(math.transpose(layer1), layer2_delta))
+const hlSizes = [(x.size())[1], (x.size())[1] - 1, (x.size())[1] + 1]
+
+for (hlSizeIndex = 0; hlSizeIndex < 3; hlSizeIndex++) {
+
+    // Weights
+    hiddenLayerSize = hlSizes[hlSizeIndex]
+    synapse0 = math.random([3, hiddenLayerSize], -1, 1)
+    synapse1 = math.random([hiddenLayerSize, 1], -1, 1)
+
+    for (let i = 0; i < 60000; i++) {
+        // forward propagation
+        layer1 = math.multiply(layer0, synapse0).map(sigmoid)
+        layer2 = math.multiply(layer1, synapse1).map(sigmoid)
+        // compare estimate with actual output
+        layer2_error = math.subtract(y, layer2)
+        // use slope of sigmoid to update values
+        layer2_delta = multiplyElements(layer2_error, layer2.map(deriv))
+        // distribute error over synapse 1
+        layer1_error = math.multiply(layer2_delta, math.transpose(synapse1))
+        // use slope of sigmoid to update values
+        layer1_delta = multiplyElements(layer1_error, layer1.map(deriv))
+        // compare estimate with actual output
+        // update weights
+        synapse0 = math.add(synapse0, math.multiply(math.transpose(layer0), layer1_delta))
+        synapse1 = math.add(synapse1, math.multiply(math.transpose(layer1), layer2_delta))
+    }
+    getAveError(layer2_error)
+    //print ("Error:" + str(np.mean(np.abs(l2_error))))
 }
 console.log("Output of training data after training")
 console.log(layer2)
